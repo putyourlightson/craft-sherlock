4f845a5d9107d275a453c3ebc0e8adf1a800d771
Ben Croker <ben@putyourlightson.net>
2021-01-16 10:56:12 +0100

    Added info text to settings

--- tests/unit/services/TestsTest.php
+++ tests/unit/services/TestsTest.php
@@ -16,37 +16,26 @@ use UnitTester;
 
 class TestsTest extends Unit
 {
-    private $_beforeRunTests = false;
-
     /**
      * @var UnitTester
      */
     protected $tester;
 
-    protected function _before()
+    /**
+     * Test all tests together, otherwise each test will result in new Guzzle calls.
+     */
+    public function testAllTests()
     {
-        parent::_before();
-
         Craft::$app->getSites()->getCurrentSite()->setBaseUrl('http://craft3/');
-    }
 
-    public function testHttpsControlPanel()
-    {
         $testModel = Sherlock::$plugin->tests->runTest('httpsControlPanel');
         $this->assertFalse($testModel->pass);
-    }
 
-    public function testHttpsFrontEnd()
-    {
         $testModel = Sherlock::$plugin->tests->runTest('httpsFrontEnd');
         $this->assertFalse($testModel->pass);
-    }
 
-    public function testWebAliasInSiteBaseUrl()
-    {
         $testModel = Sherlock::$plugin->tests->runTest('webAliasInSiteBaseUrl');
         $this->assertTrue($testModel->pass);
-
         Craft::$app->getRequest()->isWebAliasSetDynamically = true;
         Craft::$app->getSites()->getCurrentSite()->setBaseUrl('@web');
         $testModel = Sherlock::$plugin->tests->runTest('webAliasInSiteBaseUrl');
